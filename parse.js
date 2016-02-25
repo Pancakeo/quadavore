90ade16bb56913cde5840f03ab18da293152fafe
markle12 <markle12@gmail.com>
2016-02-25 04:05:38 +0000

    initial parse script, bower.json for dependencies (one of which is not actually a dependency yet)

---
+++ parse.js
@@ -0,0 +1,76 @@
+var CSV = require('./bower_components/comma-separated-values/csv.js');
+var fs = require('fs');
+var filename = process.argv[2];
+
+var modules = {
+    avg_speed_after_takeoff: {
+        speed_over_zero: false,
+        speed_cumulative: 0,
+        speed_records: 0,
+        per_row: function(row, index)
+        {
+            var speed = parseFloat(row['speed(mph)']);
+            
+            if (this.speed_over_zero === false && speed > 0)
+            {
+                this.speed_over_zero = true;
+            }
+            
+            if (this.speed_over_zero === true)
+            {
+                this.speed_cumulative += speed;
+                this.speed_records++;
+            }
+        },
+        result: function()
+        {
+            return this.speed_cumulative / this.speed_records;
+        }
+    },
+    avg_alt_after_takeoff: {
+        alt_over_zero: false,
+        alt_cumulative: 0,
+        alt_records: 0,
+        per_row: function(row, index)
+        {
+            var altitude = parseFloat(row['altitude(feet)']);
+            
+            if (this.alt_over_zero === false && altitude > 0)
+            {
+                this.alt_over_zero = true;
+            }
+            
+            if (this.alt_over_zero === true)
+            {
+                this.alt_cumulative += altitude;
+                this.alt_records++;
+            }
+        },
+        result: function()
+        {
+            return this.alt_cumulative / this.alt_records;
+        }
+    }
+}
+
+function parse_log(filename, modules)
+{
+    fs.readFile(filename, {encoding: 'utf8'}, function(err, content)
+    {
+        var rows = new CSV(content, { header: true}).parse();
+        rows.forEach(function(row, index)
+        {
+            for (var module in modules)
+            {
+                modules[module].per_row(row, index);
+            }
+        });
+        
+        for (var module in modules)
+        {
+            console.log(module+': '+modules[module].result());
+        }
+    });
+}
+
+parse_log(filename, modules);
